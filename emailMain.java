8f0202eb6f3d19015e474532d6cd0bd6c9e05afc
Sidhartha Paidi <paidisaisidhartha9@gmail.com>
2024-01-11 22:05:07 +0530

    add password generator method

--- emailMain.java
+++ emailMain.java
@@ -7,7 +7,10 @@ public class emailMain {
         private String password;
         private String department;
         private int mailBoxCapacity;
+        private String email;
+        private int defaultPasswordLength =10;
         private String alternateEmail;
+        private String companysuffix = "aeycompany.com";
         
         //Constructor to receive firstName and lastName
 
@@ -15,10 +18,18 @@ public class emailMain {
             this.firstName = firstName;
             this.lastName = lastName;
             System.out.println("Email created :" + firstName + " " + lastName);
-
+            
+            //Call this method asking for the department returns the department
             this.department = setDepartment();
             System.out.println("Department name is " + this.department);
             
+            //call a method that generates a random password
+            this.password = randomPassword(defaultPasswordLength);
+            System.out.println("Your password is "+this.password);
+
+            //combine element to generate email
+            this.email = firstName.toLowerCase()+"."+lastName.toLowerCase()+"@"+department+"."+companysuffix;
+            System.out.println("Your email is "+email);
         }
 
         //Ask for Department
@@ -31,7 +42,17 @@ public class emailMain {
             else if(depChoice == 3){return "accounting";}
             else{return "";}
         }
-
+        //Generate a Random Password
+        private String randomPassword(int length){
+            String passwordSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ01233456789!@#$";
+            char[] password = new char[length];
+            for(int i = 0;i<length;i++){
+                int rand = (int)(Math.random()*passwordSet.length());
+                password[i] = passwordSet.charAt(rand);
+            }
+            return new String(password);
+        }
+        //set the mail box capacity
 
 
 
